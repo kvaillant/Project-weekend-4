112fa9d3240529978ef670acfeb0653e0233b0ee
kvaillant <vaillant.karl.971@gmail.com>
2022-10-17 02:34:55 +0200

    add request voting tokens

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService, ClaimPaymentDTO, PaymentOrder } from './app.service';
 import { MintTokenDto } from './dto/mint.token.dto';
 
 @Controller()
@@ -17,6 +17,35 @@ export class AppController {
   @Get('allowance')
   getAllowance(@Query('from') from: string, @Query('to') to: string) {
     return this.appService.getAllownace(from, to);
+  }
+  @Get('transaction-by-hash/:hash')
+  getTransactionByHash(@Param('hash') hash: string) {
+    return this.appService.getTransactionByHash(hash);
+  }
+  @Get('transaction-receipt-by-hash/:hash')
+  getTransactionReceiptByHash(@Param('hash') hash: string) {
+    return this.appService.getTransactionReceiptByHash(hash);
+  }
+
+  @Post('create-order')
+  createOrder(@Body() body: PaymentOrder) {
+    this.appService.createPaymentOrder(body);
+  }
+  @Get('list-payment-orders')
+  getListPaymentOrders() {
+    return this.appService.listPaymentOrders();
+  }
+  @Get('get-payment-order')
+  getPaymentOrder(@Query('id') id: string) {
+    return this.appService.getPaymentOrderById(id);
+  }
+  @Post('claim-payment')
+  claimPayment(@Body() body: ClaimPaymentDTO) {
+    return this.appService.claimPayment(body);
+  }
+  @Post('request-voting-tokens')
+  requestTokesn(@Body() body: any) {
+    return this.appService.requestTokens(body);
   }
   @Post('mint-tokens')
   mintTokens(@Body() payload: MintTokenDto) {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
 import * as MyERC20Votes from './assets/MyERC20Votes.json';
 import * as TokenizedBallot from './assets/TokenizedBallot.json';
 
 const MYERC20Votes_ADDRESS = '0xbed2b6c106a1b60d1a63fd71a4bc24cb8d8808cc';
 const TokenizedBallot_ADDRESS = '0x79aa6c0376a693d66ec6c7347c9c1e19a9f7d283';
+export class PaymentOrder {
+  id: string;
+  secret: string;
+  amount: number;
+}
+export class ClaimPaymentDTO {
+  id: string;
+  secret: string;
+  address: string;
+}
 
+export class TokenRequest {
+  message: string;
+  signature: string;
+}
 @Injectable()
 export class AppService {
   signer: ethers.Wallet;
   provider: ethers.providers.BaseProvider;
+  database: PaymentOrder[];
   myERC20VotesContract: ethers.Contract;
   tokenizedBallotContract: ethers.Contract;
   constructor() {
@@ -26,6 +41,7 @@ export class AppService {
       TokenizedBallot.abi,
       this.provider,
     );
+    this.database = [];
   }
   getTokenAddress() {
     return { address: MYERC20Votes_ADDRESS };
@@ -58,6 +74,66 @@ export class AppService {
     // delegate voting power here!
     const tx = await this.myERC20VotesContract.delegate(to);
     return tx;
+  }
+
+  async getTransactionByHash(hash: string) {
+    return this.provider.getTransaction(hash);
+  }
+
+  async getTransactionReceiptByHash(hash: string) {
+    const tx = await this.getTransactionByHash(hash);
+    return await tx.wait();
+  }
+  async createPaymentOrder(body: PaymentOrder) {
+    this.database.push(body);
+  }
+
+  getPaymentOrderById(id: string) {
+    const element = this.database.find((entry) => entry.id);
+    return element
+      ? {
+          id: element.id,
+          amount: element.amount,
+        }
+      : undefined;
+  }
+
+  listPaymentOrders() {
+    return this.database.map((entry) => ({
+      id: entry.id,
+      amount: entry.amount,
+    }));
+  }
+
+  async claimPayment(body: ClaimPaymentDTO) {
+    const element = this.database.find((entry) => entry.id === body.id);
+    if (!element) throw new HttpException('Not Found', 404);
+    if (body.secret !== element.secret) return false;
+    const tx = await this.myERC20VotesContract.mint(
+      body.address,
+      ethers.utils.parseEther(element.amount.toString()),
+    );
+    return tx;
+  }
+
+  async requestTokens(body: TokenRequest) {
+    const { message, signature } = body;
+    console.info({ message, signature });
+    let address: string;
+    try {
+      address = ethers.utils.verifyMessage(message, signature); //
+    } catch (err) {
+      return {
+        result: false,
+      };
+    }
+    const tx = await this.myERC20VotesContract.mint(
+      address,
+      ethers.utils.parseEther('1.0'),
+    );
+    return {
+      result: true,
+    };
   }
 
   async getVoteBalance(address: string) {
